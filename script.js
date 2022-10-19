b62effa53c7d6c2d4683fd56d2b265e9d180ddea
May <aira.1996@yandex.ru>
2022-10-20 02:37:01 +0300

    change image size

--- script.js
+++ script.js
@@ -6,8 +6,22 @@ function getRandomElement(arr) {
 
 let button = document.querySelector('.button');
 let advice = document.querySelector('.advice');
+let image = document.querySelector('.image');
+let body = document.querySelector('.body');
 button.addEventListener('click', function() {
     smoothly(advice, 'textContent', getRandomElement(phrases));
     
     console.log(getRandomElement(phrases));
 })
+
+let borders = body.getBoundingClientRect();
+let width = borders.width;
+let height = borders.height;
+console.log('ширина' + width);
+if (width >= height) {
+    image.setAttribute('style', 'width: 30%');
+    console.log(width);
+} else {
+    image.setAttribute('style', 'width: 30vh');
+    console.log(width);
+}
